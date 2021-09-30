39fd4196e423a77ae9d45e1720a280bfbfefbd40
ThiagoFr4nc0 <thiagofranco88@hotmail.com>
2021-09-30 20:51:40 -0300

    :tada: LIST-01
    first commit

---
+++ cmake-build-debug/CMakeFiles/Projeto_Lista_Telefonica.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projeto_Lista_Telefonica.dir/main.c.o"
+  "Projeto_Lista_Telefonica"
+  "Projeto_Lista_Telefonica.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projeto_Lista_Telefonica.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
